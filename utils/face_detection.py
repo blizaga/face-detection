49c102aa6b5780d4c6fd986060b7e55ea6ed254a
farizalmustaqim <farizbmt@gmail.com>
2024-05-07 09:50:54 +0700

    fix to deploy to streamlit

--- utils/face_detection.py
+++ utils/face_detection.py
@@ -1,12 +1,19 @@
 from ultralytics import YOLO
 import cv2
 import os
+import numpy as np
 
 FACE_DETECTION_PATH = os.path.join("models", "yolov8n-face.pt")
 
+# function to convert file buffer to cv2 image
+def create_opencv_image_from_stringio(img_stream, cv2_img_flag=1):
+    img_stream.seek(0)
+    img_array = np.asarray(bytearray(img_stream.read()), dtype=np.uint8)
+    return cv2.imdecode(img_array, cv2_img_flag)
+
 # Make class for face detection with image and save the inference image
 class FaceDetection:
-    def __init__(self, image_path, device="cpu"):
+    def __init__(self, img_file_buffer, device="cpu"):
         """
         Initialize the FaceDetection class.
 
@@ -15,7 +22,7 @@ class FaceDetection:
             model_path (str): The path to the YOLOv8 model file.
             device (str, optional): The device to run the model on. Defaults to "cpu".
         """
-        self.image_path = image_path
+        self.img_file_buffer = img_file_buffer
         self.device = device
 
     def detect_face(self):
@@ -29,10 +36,23 @@ class FaceDetection:
         model = YOLO(FACE_DETECTION_PATH)
 
         # Read the image
-        image = cv2.imread(self.image_path)
+        open_cv_image = create_opencv_image_from_stringio(self.img_file_buffer)
+
 
         # Run YOLOv8 on the image
-        result = model.predict(image, device=self.device, verbose=True)[0]
+        prediction = model.predict(source=open_cv_image, device=self.device, verbose=True)[0]
 
-        return result
+        for result in prediction:
+            # Draw bounding box
+            boxes = result.boxes
+
+            for box in boxes:
+                x1, y1, x2, y2 = np.squeeze(box.xyxy.tolist())
+                x1 = int(x1)
+                y1 = int(y1)
+                x2 = int(x2)
+                y2 = int(y2)
+                cv2.rectangle(open_cv_image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+        return open_cv_image
 
